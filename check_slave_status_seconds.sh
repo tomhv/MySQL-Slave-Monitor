57509be01af4f4b299554d6e5cb6dd811f7cc0a6
Tom Haskins-Vaughan <tomhv@janeiredale.com>
2011-08-08 10:09:38 -0400

    Made some changes to run on mysqlslave

---
+++ check_slave_status_seconds.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# This script monitors the number or seconds the slave is behind the master and
+# writes it to the main log
+#
+# @author Tom Haskins-Vaughan <tomh@janeiredale.com>
+# @since  2011-07-11
+
+# Load config files
+source config.defaults
+source config.local
+
+SECONDS=`echo "SHOW SLAVE STATUS\G" | mysql -u $SLAVE_USER -p$SLAVE_PASSWORD -h $SLAVE_HOST | grep Seconds_Behind_Master | awk '{ print $2 }'`
+
+echo "`date` $SECONDS_BEHIND_LABEL: $SECONDS" >> $LOG_FILE
--- monitor_mysql_replication.sh
+++ monitor_mysql_replication.sh
@@ -13,29 +13,29 @@ source config.local
 for i in {1..9}
 do
 
-  io_error_code=`echo "SHOW SLAVE STATUS\G" | mysql -u slave_check -p$SLAVE_PASSWORD | grep Last_IO_Errno | sed 's/[^0-9]//g'`
-  sql_error_code=`echo "SHOW SLAVE STATUS\G" | mysql -u slave_check -p$SLAVE_PASSWORD | grep Last_SQL_Errno | sed 's/[^0-9]//g'`
+  IO_ERROR_CODE=`echo "SHOW SLAVE STATUS\G"  | mysql -u $SLAVE_USER -p$SLAVE_PASSWORD -h $SLAVE_HOST | grep Last_IO_Errno  | sed 's/[^0-9]//g'`
+  SQL_ERROR_CODE=`echo "SHOW SLAVE STATUS\G" | mysql -u $SLAVE_USER -p$SLAVE_PASSWORD -h $SLAVE_HOST | grep Last_SQL_Errno | sed 's/[^0-9]//g'`
 
-  if [ $io_error_code == "0" -a $sql_error_code == "0" ]; then
+  if [ $IO_ERROR_CODE == "0" -a $SQL_ERROR_CODE == "0" ]; then
     # Remove all lines from status file except for one
-    echo "OK" > $STATUS_FILE
-  
+    echo $STATUS_OK > $STATUS_FILE
+
     # Remove the suppression file
-    if [ -e $suppression_file ]; then
-      rm $suppression_file
+    if [ -e $EMAIL_SUPPRESSION_FILE ]; then
+      rm $EMAIL_SUPPRESSION_FILE
     fi
   else
     # Log the errors and update the status file
-    echo "SHOW SLAVE STATUS\G" | mysql -u slave_check -p$SLAVE_PASSWORD | grep Err | awk '{print d,$1,$2;}' "d=$(date)" >> $STATUS_FILE
-    echo "SHOW SLAVE STATUS\G" | mysql -u slave_check -p$SLAVE_PASSWORD | grep Err | awk '{print d,$1,$2;}' "d=$(date)" >> $log_file
+    echo "SHOW SLAVE STATUS\G" | mysql -u $SLAVE_USER -p$SLAVE_PASSWORD -h $SLAVE_HOST | grep Err | awk '{print d,$1,$2;}' "d=$(date)" >> $STATUS_FILE
+    echo "SHOW SLAVE STATUS\G" | mysql -u $SLAVE_USER -p$SLAVE_PASSWORD -h $SLAVE_HOST | grep Err | awk '{print d,$1,$2;}' "d=$(date)" >> $LOG_FILE
   fi
 
   # Only send an email if we have had 3 error codes, i.e. 18 lines
   if [ `cat $STATUS_FILE | wc -l` -gt 18 ]; then
     # Only send one email
-    if [ ! -e $suppression_file ]; then
+    if [ ! -e $EMAIL_SUPPRESSION_FILE ]; then
       cat $STATUS_FILE | mail --subject="MySQL Master-Slave Error" $ERROR_EMAIL_RECIPIENTS
-      touch $suppression_file
+      touch $EMAIL_SUPPRESSION_FILE
     fi 
   fi
 
